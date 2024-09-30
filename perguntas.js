3df29274efa44f2198bb09e70cf1a0d6e7bb9b78
theus12345 <133248673+theus12345@users.noreply.github.com>
2024-09-30 13:17:00 -0300

    30/09

---
+++ perguntas.js
@@ -0,0 +1,22 @@
+criaCartao(
+'programação,'
+'O que é Python?' 
+'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+'geografia'
+'qual a capital da França?',
+'A capital da França é Paris'
+)
+
+criaCartao(
+'programação'
+' o que é uma função?',
+' uma função é um bloco de código que executa alguma tarefa'
+)
+criaCartao(
+'lingua inglesa'
+'como se diz oi em ingles?',
+ ' oi em ingles é HI (RAI)'
+)
